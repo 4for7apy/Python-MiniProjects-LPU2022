b7601c64bd56b8f70f5f0551c3b95a63d8c6f2a8
Arpit srivastav <114863914+4for7apy@users.noreply.github.com>
2022-11-24 19:41:58 +0530

    Update Project-2.py

--- Projects/Project-2.py
+++ Projects/Project-2.py
@@ -1,60 +1,61 @@
 # Rock Paper Scissor
 
-import time
-from random import *
-from os import system
-print("\n \n Welcome To The Game Of Rock Paper And Scissor \n First to Score 3 Points wins!")
-c=h=0
-options=["Scissors","Paper","Rock"]
-while(c!=3 and h!=3):
-    print("\n \n Press: \n 0 for Scissor \n 1 for Rock \n 2 for Rock \n")
-    g=int(input())
-    gc=randint(0,2)
-    if(g>2):
-        system('cls')
-        print("\n \n Invalid Input try again!")
-        print("\n \nScore Board: \n ---------------- \n| Human Score:",h,"|"," \n ---------------- \n| Comp  Score:",c,"|\n ---------------- \n")
-        continue
-    system('cls')
-    match options[g]:
-        case "Scissors":
-            if(gc==0):
-                print("\n \n Draw try again!")
-            elif(gc==1):
-                print("\n \n Human : Scissor Cuts Through Paper!")
-                print("\n User +1 Point")
-                h+=1
-            else:
-                print("\n \n CPU : Rock Smashes Scissor!")
-                print("\n Comp +1 Point")
-                c+=1
-        case "Paper":
-            if(gc==0):
-                print("\n \n CPU : Scissor Cuts Through Paper!")
-                print("\n Comp +1 Point")
-                c+=1
-            elif(gc==1):
-                print("\n \n Draw try again!")
-                
-            else:
-                print("\n \n Human : Paper Covers Rock!")
-                print("\n User +1 Point")
-                h+=1
-        case "Rock":
-            if(gc==0):
-                print("\n \n Human : Rock Smashes Scissor!")
-                print("\n User +1 Point")
-                h+=1
-            elif(gc==1):
-                print("\n \n CPU : Scissor Cuts Through Paper!")
-                print("\n Comp +1 Point")
-                c+=1
-            else:
-                print("\n \n Draw try again!")
-    print("\n \nScore Board: \n ---------------- \n| Human Score:",h,"|"," \n ---------------- \n| Comp  Score:",c,"|\n ---------------- \n")
-    time.sleep(1)
-if(h==3):
-    print("Human Wins! \n \n")
-else:
-    print("Computer Wins! \n \n ")                
-            
+import random, sys
+
+print("Let's Play ROCK PAPER SCISSORS GAME!")
+
+wins = 0
+losses = 0
+ties = 0
+
+while True:
+    print("Current streak: %s Wins, %s Losses, %s Ties" % (wins, losses, ties))
+    while True:
+        print("Type 'Q' to quit \n'R' for ROCK, 'P' for PAPER, 'S' for SCISSORS")
+        playermove = input().upper()
+        if playermove == "Q":
+            sys.exit()
+        if playermove == "R" or playermove == "P" or playermove == "S":
+            break
+
+    if playermove == "R":
+        print("ROCK versus...")
+    if playermove == "P":
+        print("PAPER versus...")
+    if playermove == "S":
+        print("SCISSORS versus...")
+
+    randomNum = random.randint(1, 3)
+    if randomNum == 1:
+        compMove = "R"
+        print("ROCK")
+    if randomNum == 2:
+        compMove = "P"
+        print("PAPER")
+    if randomNum == 3:
+        compMove = "S"
+        print("SCISSORS")
+
+    if playermove == compMove:
+        print("It's a tie!")
+        ties += 1
+    elif playermove == "R" and compMove == "P":
+        print("It's a loss!")
+        losses += 1
+    elif playermove == "R" and compMove == "S":
+        print("It's a win!")
+        wins += 1
+    elif playermove == "P" and compMove == "S":
+        print("It's a loss!")
+        losses += 1
+    elif playermove == "P" and compMove == "R":
+        print("It's a win!")
+        wins += 1
+    elif playermove == "S" and compMove == "R":
+        print("It's a loss!")
+        losses += 1
+    elif playermove == "S" and compMove == "P":
+        print("It's a win!")
+        wins += 1
+    else:
+        print("Thanks for trying my game")
